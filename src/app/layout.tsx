03842cd25613dd94aa534f55f06100d331ba5e97
Mayank <mayanksingh6787@gmail.com>
2024-08-28 23:40:11 +0530

    correction for the frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} cz-shortcut-listen="false">
         <Navbar />
-        {children}
+
         <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
           <Footer />
