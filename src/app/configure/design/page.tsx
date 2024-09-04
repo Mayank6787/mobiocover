d051d5df2ae6a3d0ba95c444820a734fc52bfc7f
Mayank <mayanksingh6787@gmail.com>
2024-09-04 16:48:45 +0530

    minimumerror

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,6 +1,10 @@
 //This is a sever side component that why we use async
 
-//what we need , 
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+//what we need ,
 /* 
 Image: 
 --url
@@ -8,8 +12,37 @@ Image:
 --width
 --croppedImageUrl
 */
-const Page =async () =>{
 
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
 
-export default Page
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
+};
+
+export default Page;
